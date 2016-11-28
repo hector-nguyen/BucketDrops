88077de0a41b6a40c3cbc502c305ab30c4c09853
hectornguyen <hectornguyen91@gmail.com>
2016-11-28 11:22:55 +0700

    Add Toolbar Add background image and Glide library

--- app/src/main/java/com/greensofts/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/greensofts/bucketdrops/ActivityMain.java
@@ -2,12 +2,28 @@ package com.greensofts.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
+
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        initBackgroundImage();
+    }
+
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this).load(R.drawable.background)
+                .centerCrop()
+                .into(background);
     }
 }
